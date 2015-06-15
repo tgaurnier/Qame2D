b1a75d15eea7a19eedae3ee9adb305485e5f3ef1
Tory Gaurnier <tory.gaurnier@linuxmail.org>
2015-06-15 13:49:04 -0700

    Added abstract Game class

---
+++ src/Game/Game.cpp
@@ -0,0 +1,22 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Game.cpp                                                                   *
+ *                                                                             *
+ * Copyright (c) 2015 Tory Gaurnier                                            *
+ *                                                                             *
+ * Distributed under the MIT License.                                          *
+ * See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+#include "Game.hpp"
+
+#include "Event/GameEvent.hpp"
+#include "Event/GameEventManager.hpp"
+
+
+Game::Game() {
+	connect(
+		GameEventManager::getInstance(), SIGNAL(eventDispatched(GameEvent *)),
+		this, SLOT(manageGameEvent(GameEvent *))
+	);
+}
---
+++ src/Game/Game.hpp
@@ -0,0 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Game.hpp                                                                   *
+ *                                                                             *
+ * Copyright (c) 2015 Tory Gaurnier                                            *
+ *                                                                             *
+ * Distributed under the MIT License.                                          *
+ * See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+#ifndef GAME_HPP
+#define GAME_HPP
+
+
+#include <QObject>
+
+
+class GameEvent;
+
+
+class Game : public QObject {
+	Q_OBJECT
+
+
+	public:
+		Game();
+
+	public slots:
+		virtual void update() = 0;
+		virtual void manageGameEvent(GameEvent *event) = 0;
+};
+
+
+#endif
